1db35cb53ddbbe9d11903cc241bb178a81b0318d
ubombar <ufukbombar@gmail.com>
2019-01-23 00:14:59 +0300

    updated app package

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ class Main
 
     public static void main(String[] args)
     {
-
         CSVReader.CSVMatrix matrix = new CSVReader(new File("./res/lol.csv")).readMatrix();
         System.out.println(matrix.query((String[] row) -> Integer.valueOf(row[0]) >= 3));
     }
